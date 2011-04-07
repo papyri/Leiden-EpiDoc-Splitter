1da6d83e312cb31513ea387aead8b17baeb48278
Hugh A. Cayless <hcayless@hugh-caylesss-macbook-pro.local>
2011-04-07 11:23:31 -0400

    Initial commit of Leiden+ / EpiDoc splitter code.

---
+++ src/test/java/info/papyri/leidensplitter/EpiDocSplitterTest.java
@@ -0,0 +1,83 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.papyri.leidensplitter;
+
+import java.io.*;
+import java.util.List;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author hcayless
+ */
+public class EpiDocSplitterTest extends TestCase {
+
+  public EpiDocSplitterTest(String testName) {
+    super(testName);
+    try {
+      file = loadTestFile();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+  String file;
+
+  /**
+   * Test of split method, of class EpiDocSplitter.
+   */
+  public void testSplit_String() throws Exception {
+    System.out.println("split(String in)");
+    String in = file;
+    EpiDocSplitter instance = new EpiDocSplitter();
+    List result = instance.split(in);
+    assertEquals(in, instance.join(result));
+  }
+
+  /**
+   * Test of split method, of class EpiDocSplitter.
+   */
+  public void testSplit_File() throws Exception {
+    System.out.println("split(File in)");
+    File in = new File("/Users/hcayless/Development/APIS/leidensplitter/src/test/resources/badtext.xml");
+    EpiDocSplitter instance = new EpiDocSplitter();
+    List<String> result = instance.split(in);
+    assertEquals(file, instance.join(result));
+  }
+
+  /**
+   * Test of split method, of class EpiDocSplitter.
+   */
+  public void testSplit_Reader() throws Exception {
+    System.out.println("split(Reader in)");
+    Reader reader = new FileReader(new File("/Users/hcayless/Development/APIS/leidensplitter/src/test/resources/badtext.xml"));
+    EpiDocSplitter instance = new EpiDocSplitter();
+    List result = instance.split(reader);
+    assertEquals(file, instance.join(result));
+  }
+
+  /**
+   * Test of join method, of class EpiDocSplitter.
+   */
+  public void testJoin() throws Exception {
+    System.out.println("join");
+    EpiDocSplitter instance = new EpiDocSplitter();
+    List<String> in = instance.split(file);
+    String expResult = file;
+    String result = instance.join(in);
+    assertEquals(expResult, result);
+  }
+
+  private String loadTestFile() throws Exception {
+    File in = new File("/Users/hcayless/Development/APIS/leidensplitter/src/test/resources/badtext.xml");
+    Reader reader = new FileReader(in);
+    char[] buffer = new char[1024];
+    int read = -1;
+    StringBuilder file = new StringBuilder();
+    while ((read = reader.read(buffer)) > 0) {
+      file.append(buffer, 0, read);
+    }
+    return file.toString();
+  }
+}
